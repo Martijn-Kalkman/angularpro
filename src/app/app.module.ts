873f4572a2d3fbfb01b96d9f472a25d8f4267d3a
Peter Kalkman <peter.kalkman@partners4it.nl>
2022-02-28 13:52:36 +0100

    begin met Tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import * as fromComponents from './components/index';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...fromComponents.components
   ],
   imports: [
     BrowserModule
